b42149a5be2a52f7cdaf379c1fd4c6ce1efd4b70
Christian Kolking <christian.kolking@telus.com>
2018-12-04 23:34:35 -0500

    Ch01-01 files updated to accommodate Ch01-02,03 updates.

--- Exercise Files/Ch01/01_01/ModelData/app/src/main/java/com/example/android/data/MainActivity.java
+++ Exercise Files/Ch01/01_01/ModelData/app/src/main/java/com/example/android/data/MainActivity.java
@@ -2,20 +2,56 @@ package com.example.android.data;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+// import android.widget.TextView;
+
+import com.example.android.data.model.DataItem;
+import com.example.android.data.sample.SampleDataProvider;
+
+import java.util.ArrayList;
+import java.util.Collections;
+// Ch01-02  import java.util.Comparator;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-        TextView tvOut;
-
+ //Ch01-02       TextView tvOut;
+        List<DataItem> dataItemList = SampleDataProvider.dataItemList;
+        List<String> dataItemNames = new ArrayList<>();
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
 
-            tvOut = (TextView) findViewById(R.id.out);
-            tvOut.setText(R.string.ready_to_code);
+            // Ch01-01:
+            // DataItem item = new DataItem(null, "My Menu Item", "a cateogry", "description stuff", 1, 10.55, "cake ciocia");
 
+            // Ch01-02: tvOut = (TextView) findViewById(R.id.out);
+            // Ch01-01:
+            // tvOut.setText(item.toString());
+
+            // Ch01-02:
+/* Ch01-02
+            tvOut.setText("");
+            Collections.sort(dataItemList, new Comparator<DataItem>() {
+                @Override
+                public int compare(DataItem o1, DataItem o2) {
+                    return o1.getItemName().compareTo(o2.getItemName());
+                }
+            });
+*/
+            for (DataItem item : dataItemList) {
+                //Ch01-02  tvOut.append(item.getItemName() + "\n");
+                dataItemNames.add(item.getItemName());
+            }
+            Collections.sort(dataItemNames);  // no custom comparator needed as opposed to Ch01-02 code.
+
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                    this, android.R.layout.simple_list_item_1, dataItemNames);
+
+            ListView listView = (ListView) findViewById(android.R.id.list);
+            listView.setAdapter(adapter);
         }
 }
---
+++ Exercise Files/Ch01/01_01/ModelData/app/src/main/java/com/example/android/data/model/DataItem.java
@@ -0,0 +1,96 @@
+package com.example.android.data.model;
+
+public class DataItem {
+
+    // handy to use only String and Primitive data types for the class to pass around.
+    private String itemId;  // common practice to use String as a GUID, later used to sync local stored data and remote DB.
+    private String itemName;
+    private String description;
+    private String category;
+    private int sortPosition;
+    private double price;
+    private String image;
+
+    public DataItem() {
+    }
+
+    public DataItem(String itemId, String itemName, String description, String category, int sortPosition, double price, String image) {
+        this.itemId = itemId;
+        this.itemName = itemName;
+        this.description = description;
+        this.category = category;
+        this.sortPosition = sortPosition;
+        this.price = price;
+        this.image = image;
+    }
+
+    public String getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(String itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getSortPosition() {
+        return sortPosition;
+    }
+
+    public void setSortPosition(int sortPosition) {
+        this.sortPosition = sortPosition;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    @Override
+    public String toString() {
+        return "DataItem{" +
+                "itemId='" + itemId + '\'' +
+                ", itemName='" + itemName + '\'' +
+                ", description='" + description + '\'' +
+                ", category='" + category + '\'' +
+                ", sortPosition=" + sortPosition +
+                ", price=" + price +
+                ", image='" + image + '\'' +
+                '}';
+    }
+
+}
